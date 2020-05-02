bfe90734504a86cc2af546a0a469763d168c043a
ChegeDaniella <chegedaniella@gmail>
2020-05-02 14:20:44 +0300

    initializing flask login

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,6 +2,11 @@ from flask import Blueprint,Flask
 main = Blueprint('main',__name__)
 from app.main import views
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap= Bootstrap()
 
@@ -10,6 +15,7 @@ def create_app():
 
     #initializing flask extensions
     bootstrap.init_app(app)
+    login_manager.init_app(app)
 
     #Registering the blueprint
     from main import main as main_blueprint
